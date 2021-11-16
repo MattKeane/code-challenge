4db638b4e8b91374aa7a143b43abaf7cb60eaf19
Matt Keane <mattpkeane@gmail.com>
2021-11-16 13:31:54 -0600

    api now returns JSON of parsed address"

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
-
+import json
 
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
@@ -16,7 +16,14 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = request.query_params['address']
+        address_components, address_type = self.parse(input_string)
+        parse_address_data = {
+            'input_string': input_string, 
+            'address_components': address_components, 
+            'address_type': address_type
+        }
+        return Response(parse_address_data)
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
