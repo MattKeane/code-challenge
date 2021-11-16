bac9477e79415aec5e856390141432fc31216a07
Matt Keane <mattpkeane@gmail.com>
2021-11-16 16:26:44 -0600

    step 5 incomplete, not sure what's going with my test requests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,15 @@
 import pytest
 
-
 def test_api_parse_succeeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
+    print(address_string)
+    url = '/api/parse'
+    response = client.get(url, {'address': address_string})
+    assert response.status_code == 200
+    # this keeps failing I'm not quite sure why
+    # clearly there's something wrong with the request I'm making
     pytest.fail()
 
 
