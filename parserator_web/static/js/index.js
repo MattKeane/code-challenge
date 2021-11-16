e792c468dd6d47733549a196e4fccd9e878eab9a
Matt Keane <mattpkeane@gmail.com>
2021-11-16 14:10:18 -0600

    completed step 3

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,17 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+(() => {
+   const API_URL = 'http://localhost:8000/'
+   // IRL I would use an environment variable for this
+   const submitButton = document.querySelector('#submit');
+   const addressInput = document.querySelector('#address');
+
+   submitButton.addEventListener('click', async e => {
+      e.preventDefault();
+      const url = API_URL + 'api/parse?address=' + addressInput.value.replace(/\s/g, '+');
+      const parseAddressResponse = await fetch(url);
+      const parseAddressJson = await parseAddressResponse.json();
+      console.log(parseAddressJson);
+   });
+})();
